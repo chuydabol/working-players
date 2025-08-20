d63d12923a48c5e91befa16f224178eb65537503
chuydabol <chuylealrosa1234@gmail.com>
2025-08-20 01:58:44 -0700

    chore: add firebase dependencies

--- server.js
+++ server.js
@@ -6,10 +6,12 @@ const cors = require('cors');
 const cron = require('node-cron');
 
 const app = express(); // ✅ Initialize Express
-const serviceAccount = require('./config/firebase-service-account.json');
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
 });
 
 const db = admin.firestore(); // ✅ Firestore setup
